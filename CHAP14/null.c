d51bd9a9e5c30caab6ff8c24358b029e6a9491a5
JongKI-PARK <97037853+JongKI-PARK@users.noreply.github.com>
2022-09-23 12:31:16 +0900

    Add files via upload

---
+++ CHAP14/null.c
@@ -0,0 +1,13 @@
+//null.c : creates a pointer to an integer
+//sets it to NULL and try to dereference it
+//2022.8.16 park jong ki
+//
+#include <stdio.h>
+
+int main() {
+	
+	int* pointer = NULL;
+	int i = *pointer;
+
+	return 0;
+}
